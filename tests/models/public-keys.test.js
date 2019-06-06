c0d51b70187504d3e7b74b4139b60634a91ee349
Matthew Smith <m@lattejed.com>
2019-06-06 13:38:01 +0700

    Fixed tests for public keys

--- tests/models/public-keys.test.js
+++ tests/models/public-keys.test.js
@@ -1,92 +1,84 @@
 
 const assert = require('assert')
-//const {testKeys} = require('../test-data')
+const {hsmKey, tzKey} = require('../data/all')
 const {getPublicKeyFromXY} = require('../../models/public-key')
 
 const PK = require('../../constants/pubkey-constants')
-/*
+
 describe('models/public-keys', () => {
 
   describe('getPublicKeyFromXY', () => {
 
     describe('Curve P256', () => {
 
-      let key = testKeys.P256
+      let key = hsmKey.p256
       let {x, y, crv} = key.key
 
       it('Should create valid PK.UNCOMPRESSED', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.UNCOMPRESSED).toString('hex')
-        let key2 = key.publicKeyU
+        let key2 = tzKey.p256.uncompressed
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.COMPRESSED', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.COMPRESSED).toString('hex')
-        let key2 = key.publicKeyC
+        let key2 = tzKey.p256.compressed
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.TEZOS', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.TEZOS).toString('hex')
-        let key2 = key.publicKeyTZ
+        let key2 = tzKey.p256.pk
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.TEZOS_HASH', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.TEZOS_HASH).toString('hex')
-        let key2 = key.publicKeyTZH
+        let key2 = tzKey.p256.pkh
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
     })
 
     describe('Curve P256K', () => {
 
-      let key = testKeys.P256K
+      let key = hsmKey.p256k
       let {x, y, crv} = key.key
 
       it('Should create valid PK.UNCOMPRESSED', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.UNCOMPRESSED).toString('hex')
-        let key2 = key.publicKeyU
+        let key2 = tzKey.p256k.uncompressed
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.COMPRESSED', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.COMPRESSED).toString('hex')
-        let key2 = key.publicKeyC
+        let key2 = tzKey.p256k.compressed
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.TEZOS', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.TEZOS).toString('hex')
-        let key2 = key.publicKeyTZ
+        let key2 = tzKey.p256k.pk
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
       it('Should create valid PK.TEZOS_HASH', () => {
 
         let key1 = getPublicKeyFromXY(x, y, crv, PK.TEZOS_HASH).toString('hex')
-        let key2 = key.publicKeyTZH
+        let key2 = tzKey.p256k.pkh
         assert(key1 === key2, `${key1} should equal ${key2}`)
-
       })
 
     })
 
   })
+  
 })
-*/
